85330745107344359e0d89ad3ac4badf8de51401
ashutosh birthare <86846323+ashuashu2@users.noreply.github.com>
2024-01-10 14:15:59 +0530

    update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,50 +1,42 @@
 import axios from "axios";
-import "./Login.css"
+import "./Login.css";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { loginHandler, signupHandler } from "../../Redux Management/features/authSlice/AuthServices";
+import {
+  loginHandler,
+  signupHandler,
+} from "../../Redux Management/features/authSlice/AuthServices";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import ashu from "../../images/ashu.jpg"
-
+import ashu from "../../images/ashu.jpg";
 
 function LoginComponent() {
-
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const location = useLocation();
-  const from = location?.state?.from.pathname
-
-
-
+  const from = location?.state?.from.pathname;
 
   async function loginButtonHandler() {
     const data = {
       username: username,
-      password: password
-    }
+      password: password,
+    };
     try {
       if (username.length >= 1 && password.length >= 1) {
         dispatch(loginHandler(data)).then(() => {
           setTimeout(() => {
             navigate(from ? from : "/");
-            toast.success(`login succesfully! Welcome ${username} `)
-
+            toast.success(`login succesfully! Welcome ${username} `);
           }, 500);
-        })
-
+        });
       } else {
-        toast.info("please fill all the credentials")
+        toast.info("please fill all the credentials");
       }
-
-
     } catch (error) {
-      console.log(error)
-
+      console.log(error);
     }
-
   }
 
   function guestButtonHandler() {
@@ -64,8 +56,7 @@ function LoginComponent() {
       const loginData = {
         username: "ashutosh",
         password: "ashu@shu12",
-
-      }
+      };
 
       try {
         dispatch(signupHandler(data))
@@ -73,58 +64,64 @@ function LoginComponent() {
           .then(() => {
             setTimeout(() => {
               navigate(from ? from : "/");
-              toast.success(`login succesfully! Welcome Again ${loginData.username} `)
-
+              toast.success(
+                `login succesfully! Welcome Again ${loginData.username} `
+              );
             }, 500);
           })
           .catch(() => {
-            window.location.reload()
-            toast.warning("please try again")
-
-
-          })
-
-
+            window.location.reload();
+            toast.warning("please try again");
+          });
       } catch (error) {
-        console.log(error)
-
+        console.log(error);
       }
     }
   }
 
-
   return (
     <div className="login-main-div">
       <div className="login-small-div">
-
-        <h2 className="login-text-div">Log-<span className="in-text-login">in</span></h2>
+        <h2 className="login-text-div">
+          Log-<span className="in-text-login">in</span>
+        </h2>
         <div className="login-input-div">
           <fieldset>
             <legend>Userame :</legend>
-            <input onChange={(e) => setUserName(e.target.value)} placeholder="Enter username" type="text" />
+            <input
+              onChange={(e) => setUserName(e.target.value)}
+              placeholder="Enter username"
+              type="text"
+            />
           </fieldset>
         </div>
 
         <div className="login-input-div">
           <fieldset>
             <legend>Password :</legend>
-            <input onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" type="text" />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter Password"
+              type="text"
+            />
           </fieldset>
         </div>
         <div className="login-button-div">
-          <div className="login-button" onClick={loginButtonHandler} >Login</div>
-          <div className="guest-button" onClick={guestButtonHandler} >Login As Guest</div>
-
+          <div className="login-button" onClick={loginButtonHandler}>
+            Login
+          </div>
+          <div className="guest-button" onClick={guestButtonHandler}>
+            Login As Guest
+          </div>
         </div>
         <div className="loginpage-signup-link">
           <h4>Dont have an account?</h4>
-          <Link className="Sign-up-here-link" to="/signup">Sign up here</Link>
+          <Link className="Sign-up-here-link" to="/signup">
+            Sign up here
+          </Link>
         </div>
-
-
       </div>
-
     </div>
-  )
+  );
 }
-export { LoginComponent } 
+export { LoginComponent };
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,109 +1,128 @@
-import "./Signup.css"
+import "./Signup.css";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { signupHandler } from "../../Redux Management/features/authSlice/AuthServices";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 function SignupComponent() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
+  async function signupButtonHandler() {
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      username: username,
+      password: password,
+      bio: "Aspiring Frontend Engineer",
+    };
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [username, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate()
-
-
-    async function signupButtonHandler() {
-        const data = {
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            username: username,
-            password: password,
-            bio: "Aspiring Frontend Engineer",
-            website: `https://${data.firstName}${data.lastName}.netlify.app/`,
-
-        }
-        try {
-            if (firstName.length >= 1 && lastName.length >= 1 && username.length >= 1 && email.length >= 1 && password.length >= 1) {
-
-                dispatch(signupHandler(data)).then(() => {
-
-                    setTimeout(() => {
-                        navigate("/login");
-                        toast.success("signup succesfully")
-
-                    }, 500);
-                })
-            } else {
-                toast.info("please fill all the credentials")
-
-            }
-        } catch (error) {
-            console.log(error)
-            toast.error("something is not right please try after some times")
-
-
-        }
+    try {
+      if (
+        firstName.length >= 1 &&
+        lastName.length >= 1 &&
+        username.length >= 1 &&
+        email.length >= 1 &&
+        password.length >= 1
+      ) {
+        dispatch(signupHandler(data)).then(() => {
+          setTimeout(() => {
+            navigate("/login");
+            toast.success("signup succesfully");
+          }, 500);
+        });
+      } else {
+        toast.info("please fill all the credentials");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("something is not right please try after some times");
     }
+  }
 
+  console.log(firstName);
+  console.log(lastName);
+  console.log(email);
+  console.log(username);
+  console.log(password);
 
-
-
-    return (
-        <div className="signup-main-div">
-            <div className="signup-small-div">
-
-                <h2 className="signup-text-div">Sign-<span className="up-text-signup">up</span> </h2>
-                <div className="signup-input-div">
-                    <fieldset>
-                        <legend>FirstName :</legend>
-                        <input onChange={(e) => setFirstName(e.target.value)} placeholder="Enter FirstName" type="text" />
-                    </fieldset>
-                </div>
-
-                <div className="signup-input-div">
-                    <fieldset>
-                        <legend>LastName :</legend>
-                        <input onChange={(e) => setLastName(e.target.value)} placeholder="Enter LastName" type="text" />
-                    </fieldset>
-                </div>
-
-                <div className="signup-input-div">
-                    <fieldset>
-                        <legend>Email :</legend>
-                        <input onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" type="text" />
-                    </fieldset>
-                </div>
-                <div className="signup-input-div">
-                    <fieldset>
-                        <legend>UserName :</legend>
-                        <input onChange={(e) => setUserName(e.target.value)} placeholder="Enter UserName" type="text" />
-                    </fieldset>
-                </div>
-
-                <div className="signup-input-div">
-                    <fieldset>
-                        <legend>Password :</legend>
-                        <input onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" type="text" />
-                    </fieldset>
-                </div>
-                <div className="signup-button-div">
-                    <button onClick={signupButtonHandler} >Sign up</button>
-                </div>
-                <div className="signuppage-login-link">
-                    <h4>Already have an account?</h4>
-                    <Link className="Sign-up-here-link" to="/login">Log in here</Link>
-                </div>
-
-
-            </div>
-
+  return (
+    <div className="signup-main-div">
+      <div className="signup-small-div">
+        <h2 className="signup-text-div">
+          Sign-<span className="up-text-signup">up</span>{" "}
+        </h2>
+        <div className="signup-input-div">
+          <fieldset>
+            <legend>FirstName :</legend>
+            <input
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="Enter FirstName"
+              type="text"
+            />
+          </fieldset>
         </div>
-    )
+
+        <div className="signup-input-div">
+          <fieldset>
+            <legend>LastName :</legend>
+            <input
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Enter LastName"
+              type="text"
+            />
+          </fieldset>
+        </div>
+
+        <div className="signup-input-div">
+          <fieldset>
+            <legend>Email :</legend>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter Email"
+              type="text"
+            />
+          </fieldset>
+        </div>
+        <div className="signup-input-div">
+          <fieldset>
+            <legend>UserName :</legend>
+            <input
+              onChange={(e) => setUserName(e.target.value)}
+              placeholder="Enter UserName"
+              type="text"
+            />
+          </fieldset>
+        </div>
+
+        <div className="signup-input-div">
+          <fieldset>
+            <legend>Password :</legend>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter Password"
+              type="text"
+            />
+          </fieldset>
+        </div>
+        <div className="signup-button-div">
+          <button onClick={signupButtonHandler}>Sign up</button>
+        </div>
+        <div className="signuppage-login-link">
+          <h4>Already have an account?</h4>
+          <Link className="Sign-up-here-link" to="/login">
+            Log in here
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
-export { SignupComponent } 
+export { SignupComponent };
